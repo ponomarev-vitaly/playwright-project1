0357e97994bf2719a15ef0c26d1286901d8390d3
ponomarev-vitaly <vp211082@gmail.com>
2024-10-14 19:32:00 -0500

    updated example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,4 +4,14 @@ test("First test", async({page})=>{
     //
     await page.goto("https://playwright.dev");
     await page.pause();
+    const title = page.locator('.navbar__inner .navbar__title');
+    await expect(title).toHaveText('Playwright');
+})
+
+test("Second test", async({page})=>{
+    //
+    await page.goto("https://the-internet.herokuapp.com/");
+    await page.pause();
+    await page.locator("text=Add/Remove Elements").click();
+    await page.locator("text=Add Element").click();
 })
