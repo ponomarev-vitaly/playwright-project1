bec247d59badb0092f9b236079ccf429e382ffb0
ponomarev-vitaly <vp211082@gmail.com>
2024-10-14 23:47:12 -0500

    test: update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,34 +1,36 @@
-const {test, expect, firefox} = require('@playwright/test');
+const { test, expect, firefox } = require("@playwright/test");
 
-test("First test @regression", async({page})=>{
+test.describe("Smoke tests", () => {
+  test("First test @regression", async ({ page }) => {
     //
     await page.goto("https://playwright.dev");
     await page.pause();
-    const title = page.locator('.navbar__inner .navbar__title');
-    await expect(title).toHaveText('Playwright');
-})
+    const title = page.locator(".navbar__inner .navbar__title");
+    await expect(title).toHaveText("Playwright");
+  });
 
-test("Second test @regression", async({page,browserName})=>{
+  test("Second test @regression", async ({ page, browserName }) => {
     //
-    test.skip(browserName === 'firefox', 'Working on the firefox fix');
+    test.skip(browserName === "firefox", "Working on the firefox fix");
     await page.goto("https://the-internet.herokuapp.com/");
     await page.pause();
     await page.locator("text=Add/Remove Elements").click();
     await page.locator("text=Add Element").click();
-})
+  });
 
-test("Duplicate test @smoke", async({page})=>{
-    //
-    await page.goto("https://the-internet.herokuapp.com/");
-    await page.pause();
-    await page.locator("text=Add/Remove Elements").click();
-    await page.locator("text=Add Element").click();
-})
-
-test("Duplicate test @regression", async({page})=>{
+  test("Duplicate test @smoke", async ({ page }) => {
     //
     await page.goto("https://the-internet.herokuapp.com/");
     await page.pause();
     await page.locator("text=Add/Remove Elements").click();
     await page.locator("text=Add Element").click();
-})
+  });
+
+  test("Duplicate test @regression", async ({ page }) => {
+    //
+    await page.goto("https://the-internet.herokuapp.com/");
+    await page.pause();
+    await page.locator("text=Add/Remove Elements").click();
+    await page.locator("text=Add Element").click();
+  });
+});
