0c120e2565f745b186bf9bede4e38a9da78d13a8
ponomarev-vitaly <vp211082@gmail.com>
2024-10-17 01:12:55 -0500

    test: update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,27 +1,31 @@
 const { test, expect, firefox } = require("@playwright/test");
 
 test.describe("Smoke tests", () => {
-  test("First test @regression", async ({ page }) => {
-    //
-    await page.goto("https://playwright.dev");
-    await page.pause();
-    const title = page.locator(".navbar__inner .navbar__title");
-    await expect(title).toHaveText("Playwright");
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
   });
+
+  //test("First test @regression", async ({ page }) => {
+  //
+  //await page.goto("https://playwright.dev");
+  //await page.pause();
+  //const title = page.locator(".navbar__inner .navbar__title");
+  //await expect(title).toHaveText("Playwright");
+  //});
 
   test("Second test @regression", async ({ page, browserName }) => {
     //
     test.skip(browserName === "firefox", "Working on the firefox fix");
-    await page.goto("/checkboxes");
+
     await page.pause();
-    // await page.locator("text=Add/Remove Elements").click();
+    await page.locator("text=Add/Remove Elements").click();
     // await page.screenshot({path: "screenshot.png"});
-    // await page.locator("text=Add Element").click();
+    await page.locator("text=Add Element").click();
   });
 
   test("Duplicate test @smoke", async ({ page }) => {
     //
-    await page.goto("/");
+    //await page.goto("/");
     await page.pause();
     await page.locator("text=Add/Remove Elements").click();
     await page.locator("text=Add Element").click();
@@ -29,7 +33,7 @@ test.describe("Smoke tests", () => {
 
   test("Duplicate test @regression", async ({ page }) => {
     //
-    await page.goto("/");
+    //await page.goto("/");
     await page.pause();
     await page.locator("text=Add/Remove Elements").click();
     await page.locator("text=Add Element").click();
