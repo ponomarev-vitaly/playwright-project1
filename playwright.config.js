d89a46a7b806c96e291ab3d059307cecc538acdb
ponomarev-vitaly <vp211082@gmail.com>
2024-10-17 00:09:22 -0500

    update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,31 @@
 const {PlaywrightTestConfig} = require ('@playwright/test');
 
 const config = {
-    
+    retries: 1,
+    timeout: 60000,
+    use: {
+        headless: false,
+        viewport: {width: 1280, height: 720},
+        video: "off",
+        screenshot: "off"
+
+    },
+
+    projects: [
+        {
+            name: 'Chrome',
+            use: {browserName: 'chromium'}
+        },
+        {
+            name: 'Firefox',
+            use: {browserName: 'firefox'}
+        },
+        {
+            name: 'Chrome',
+            use: {browserName: 'chromium'}
+        }
+
+
+
+    ]
 }
