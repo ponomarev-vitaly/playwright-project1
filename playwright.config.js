b29cdad2dea950b3b2cc7a32529ea7a6fb88f773
ponomarev-vitaly <vp211082@gmail.com>
2024-10-17 23:42:40 -0500

    update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -4,10 +4,9 @@ const MyReporter = require('./reporter');
 const config = {
     retries: 1,
     timeout: 60000,
-    // reporter: './reporter.js',
+    reporter: './reporter.js',
     use: {
-        // baseURL: "https://the-internet.herokuapp.com/",
-        baseURL: "https://demoqa.com/text-box/",
+        baseURL: "https://the-internet.herokuapp.com/",
         headless: true,
         viewport: {width: 1280, height: 720},
         video: "on",
