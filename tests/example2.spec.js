6db06478ea868bce448a238eb200798e68418826
ponomarev-vitaly <vp211082@gmail.com>
2024-10-17 23:11:08 -0500

    test: update example2.spec.js

---
+++ tests/example2.spec.js
@@ -0,0 +1,11 @@
+const { test, expect } = require("@playwright/test");
+
+test.describe("Examples", () => {
+    test("Testing locators", async ({ page }) => {
+        //
+        await page.goto("");
+        
+      });
+
+})
+
