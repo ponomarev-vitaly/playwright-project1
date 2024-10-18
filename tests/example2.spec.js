61a996d304370187528663e5d2b56662d8feb421
ponomarev-vitaly <vp211082@gmail.com>
2024-10-17 23:14:06 -0500

    test: update example2.spec.js

--- tests/example2.spec.js
+++ tests/example2.spec.js
@@ -3,7 +3,8 @@ const { test, expect } = require("@playwright/test");
 test.describe("Examples", () => {
     test("Testing locators", async ({ page }) => {
         //
-        await page.goto("");
+        await page.goto("https://demoqa.com/text-box");
+        
         
       });
 
