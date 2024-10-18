b06b8ab9613e73354759ae62236d2bc621cfc38e
ponomarev-vitaly <vp211082@gmail.com>
2024-10-17 23:34:53 -0500

    test: update example2.spec.js

--- tests/example2.spec.js
+++ tests/example2.spec.js
@@ -2,14 +2,18 @@ const { test, expect } = require("@playwright/test");
 
 test.describe("Examples", () => {
     test("Testing locators", async ({ page }) => {
-        //
-        
+        //        
         await page.goto("https://demoqa.com/text-box");
-        await page.locator('#userName').type("Test Username...");
-        await page.locator('[placeholder="name@example.com"]').type("Test Email...");
-        await page.locator('#currentAddress').type("This is the current address: ");
-        await page.locator('#permanentAddress').type("This is the permanent address: ");
-        await page.locator('button.has-text("Submit")').click();        
+        await page.pause;
+        await page.locator('#userName').type("Test Username");
+        await page.pause;
+        await page.locator('[placeholder="name@example.com"]').type("test@email.com");
+        await page.pause;
+        await page.locator('#currentAddress').type("This is the current address");
+        await page.pause;
+        await page.locator('#permanentAddress').type("This is the permanent address");
+        await page.pause;
+        await page.locator('button.has-text("Submit")').click();   
 
         
       });
